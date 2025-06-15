66a5baebe644298e4485a477452c12d32745be2f
Chen Jiaqi <69673679+chenjq2@users.noreply.github.com>
2025-06-15 08:37:36 +0800

    Update train_e.py

--- PAF/train_e.py
+++ PAF/train_e.py
@@ -513,7 +513,9 @@ def main(args):
         lr_scheduler.step(epoch + 1)
 
         classes = ["accordion","airplanes","anchor","ant","BACKGROUND_Google","barrel","bass","beaver","binocular","bonsai","brain","brontosaurus","buddha","butterfly","camera","cannon","car_side","ceiling_fan","cellphone","chair","chandelier","cougar_body","cougar_face","crab","crayfish","crocodile","crocodile_head","cup","dalmatian","dollar_bill","dolphin","dragonfly","electric_guitar","elephant","emu","euphonium","ewer","Faces_easy","ferry","flamingo","flamingo_head","garfield","gerenuk","gramophone","grand_piano","hawksbill","headphone","hedgehog","helicopter","ibis","inline_skate","joshua_tree","kangaroo","ketch","lamp","laptop","Leopards","llama","lobster","lotus","mandolin","mayfly","menorah","metronome","minaret","Motorbikes","nautilus","octopus","okapi","pagoda","panda","pigeon","pizza","platypus","pyramid","revolver","rhino","rooster","saxophone","schooner","scissors","scorpion","sea_horse","snoopy","soccer_ball","stapler","starfish","stegosaurus","stop_sign","strawberry","sunflower","tick","trilobite","umbrella","watch","water_lilly","wheelchair","wild_cat","windsor_chair","wrench","yin_yang"]
+        
         test_loss, test_acc1, test_acc5 = evaluate(model, criterion, data_loader_test, device=device, header='Test:', classes=classes)
+        
         if te_tb_writer is not None:
             if utils.is_main_process():
 
